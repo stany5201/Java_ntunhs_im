24af46ff2c269034bac797c1e7d369a03df23196
stany5201 <160557077+stany5201@users.noreply.github.com>
2024-04-23 12:10:11 +0800

    Add files via upload

---
+++ Hw8/hw8/src/hw8/Main.java
@@ -0,0 +1,14 @@
+package hw8;
+
+public class Main {
+	public static void main(String[]args) {
+		Warrior warrior=new Warrior("戰士A",100,15,5);
+		Mage mage = new Mage("法師B",80,10,50);
+		
+		mage.castSpell(warrior);
+		warrior.useShield();
+		warrior.attack(mage);
+		mage.display();
+		warrior.display();
+	}
+}
---
+++ Hw8/hw8/src/hw8/Warrior.java
@@ -0,0 +1,19 @@
+package hw8;
+
+public class Warrior extends Character {
+		private int defense;
+		
+		public Warrior(String name, int health, int attackPower, int defense) {
+			super(name, health, attackPower);
+			this.defense = defense;
+		}
+		public void useShield() {
+			defense +=10;
+			System.out.println(name="使用盾牌，防禦力增加至"+defense);
+		}
+		@Override
+		public void display() {
+			System.out.println("戰士名稱:"+name+"生命值:"+health+",攻擊力:"+attackPower+",防禦力:"+defense);
+		}
+
+}
